0a172338c8455b812218e79d085e024ab6e5dd86
Stéphane RAULT <stephane.rault@radicalspam.org>
2020-04-19 16:41:35 +0200

    add TaskState choice

--- mce_django_app/constants.py
+++ mce_django_app/constants.py
@@ -7,8 +7,16 @@ class Provider(models.TextChoices):
     GCP = "gcp"
     VMWARE = "vmware"
 
-
 class EventChangeType(models.TextChoices):
     CREATE = "create"
     UPDATE = "update"
     DELETE = "delete"
+
+class TaskState(models.TextChoices):
+    UNKNOW    = 'unknow'
+    QUEUED    = 'queued'
+    FINISHED  = 'finished'
+    FAILED    = 'failed'
+    STARTED   = 'started'
+    DEFERRED  = 'deferred'
+    SCHEDULED = 'scheduled'
