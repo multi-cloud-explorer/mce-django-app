6a6e6c24cc0aeaab5c01122370e0894a946eeb5b
Stéphane RAULT <stephane.rault@radicalspam.org>
2020-04-28 12:49:10 +0200

    fix environ settings

--- manage.py
+++ manage.py
@@ -3,8 +3,7 @@
 import os, sys
 
 def main():
-    if not os.environ.get('DJANGO_SETTINGS_MODULE'):
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_test.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_test.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- project_test/wsgi.py
+++ project_test/wsgi.py
@@ -2,7 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-if not os.environ.get('DJANGO_SETTINGS_MODULE'):
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_test.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_test.settings')
 
 application = get_wsgi_application()
