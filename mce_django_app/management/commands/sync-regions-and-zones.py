0deb3ae4171c3a40d792ef1b3046331556ef887c
Stéphane RAULT <stephane.rault@radicalspam.org>
2020-05-13 08:46:19 +0200

    add aws commands

--- mce_django_app/management/commands/sync-regions-and-zones.py
+++ mce_django_app/management/commands/sync-regions-and-zones.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 
-from mce_django_app.providers_utils import azure
+from mce_django_app.providers_utils import azure, aws
+
 
 class Command(BaseCommand):
     help = 'Synchronize Regions for All Providers'
@@ -15,13 +16,16 @@ class Command(BaseCommand):
         self.stdout.write(self.style.SUCCESS("no regions for vsphere..."))
 
     def _sync_aws(self):
-        raise NotImplementedError()
+        self.stdout.write(self.style.SUCCESS("start synchronize aws - regions..."))
+        result = aws.sync_regions()
+        msg = f"synchronized aws - regions - created[{result['created']}] - updated[{result['updated']}]"
+        self.stdout.write(self.style.SUCCESS(msg))
 
     def _sync_gcp(self):
         raise NotImplementedError()
 
     def handle(self, *args, **options):
-        self._sync_azure()
         self._sync_vsphere()
+        self._sync_azure()
         self._sync_aws()
         self._sync_gcp()
--- mce_django_app/management/commands/sync-resource-types.py
+++ mce_django_app/management/commands/sync-resource-types.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 
 from mce_django_app.providers_utils import azure
+from mce_django_app.providers_utils import aws
 from mce_django_app.providers_utils import vsphere
 
 class Command(BaseCommand):
@@ -19,7 +20,10 @@ class Command(BaseCommand):
         self.stdout.write(self.style.SUCCESS(msg))
 
     def _sync_aws(self):
-        raise NotImplementedError()
+        self.stdout.write(self.style.SUCCESS("start synchronize aws..."))
+        result = aws.sync_resource_type()
+        msg = f"synchronized aws - resources-types - created[{result['created']}] - updated[{result['updated']}]"
+        self.stdout.write(self.style.SUCCESS(msg))
 
     def _sync_gcp(self):
         raise NotImplementedError()
